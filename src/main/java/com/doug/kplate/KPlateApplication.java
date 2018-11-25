7ece65c868a1b7d8571b035a0ffc7965863cb2aa
dongjunhao <dongjunhao@youjubabai.com>
2018-11-25 10:38:20 +0800

    项目搭建

---
+++ src/main/java/com/doug/kplate/KPlateApplication.java
@@ -0,0 +1,12 @@
+package com.doug.kplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KPlateApplication.class, args);
+    }
+}
